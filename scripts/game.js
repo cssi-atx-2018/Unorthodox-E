d7de15459de9ee25622d9cea7e7c75eee0fbf74a
Demo User <demouser@MacBook-Pro.local>
2018-08-01 16:16:07 -0500

    s

--- scripts/game.js
+++ scripts/game.js
@@ -30,7 +30,7 @@ document.body.appendChild(app.view)
 //PIXI.loader takes in paths for images and renders it so that they cna be used
 //for textures
 PIXI.loader
-  .add(['images/food_truck.png',
+  .add(['images/food_truck_3.png',
         'images/neon_green.png',
         'images/pink.png',
         'images/orange.png',
@@ -236,7 +236,7 @@ function setUpCans(){
 
 function setupPlayer() {
   //this funtion sets up the player in the game
-  sprite = new PIXI.Sprite(PIXI.loader.resources['images/food_truck.png'].texture)
+  sprite = new PIXI.Sprite(PIXI.loader.resources['images/food_truck_3.png'].texture)
   sprite.anchor.y = .5
   sprite.anchor.x = .5
   sprite.x = appWidth / 2
